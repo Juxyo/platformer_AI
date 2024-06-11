7bbb66b10b0e6d536dd8c68ac4e4e7a3485058ba
Juxyo <94303202+Juxyo@users.noreply.github.com>
2024-06-11 12:27:42 +0200

    tileset + map + jump changes

--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+const JUMP_VELOCITY = -750.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -22,7 +22,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jumping"
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -31,7 +31,7 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+		velocity.x = move_toward(velocity.x, 0, 100)
 
 	move_and_slide()
 
