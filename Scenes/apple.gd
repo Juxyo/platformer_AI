c31de35caf30ad9d322fd5d0c5c802a6e7be8c05
PHE <119488291+Pierre-He@users.noreply.github.com>
2024-06-17 21:50:33 +0200

    Points system

--- Scenes/apple.gd
+++ Scenes/apple.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var game_manager = %GameManager
 
 
+
+
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		queue_free()
---
+++ label.gd
@@ -0,0 +1,4 @@
+extends Label
+
+
+var points = 0
