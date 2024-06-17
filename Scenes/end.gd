b77be909f0f4b1b15cc0cb60ae3ba4373c5bf8bc
PHE <119488291+Pierre-He@users.noreply.github.com>
2024-06-17 22:22:55 +0200

    working UI, and levels

--- Scenes/end.gd
+++ Scenes/end.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-
+@export var target_level : PackedScene
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		queue_free()
 		print("you win!")
+		get_tree().change_scene_to_packed(target_level)
+		
 
